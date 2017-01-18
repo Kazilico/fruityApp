695ad7fdc493198ace5e90c852d66de3e089594f
Josh Carrico <jcarrico138@gmail.com>
2017-01-17 19:56:12 -0500

    Changed fruit photos.

--- fruity.js
+++ fruity.js
@@ -11,16 +11,72 @@ console.log (month)
 
 var data = {"fruits":[
 			{
-				"fruitname":"apples",
-				"ripemonths": ["7", "8", "9", "10", "11"],
+				"fruitname":"mulberries",
+				"ripemonths": ["7", "8"],
+			},
+			{
+				"fruitname":"nectarines",
+				"ripemonths": ["5", "6", "7", "8", "9", "10"],
+			},
+			{
+				"fruitname":"oranges",
+				"ripemonths": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
 			},
 			{
 				"fruitname":"peaches",
-				"ripemonths": ["6", "7", "8", "9"],
+				"ripemonths": ["5", "6", "7", "8", "9", "10"],
 			},
 			{
-				"fruitname":"pears",
-				"ripemonths": ["8", "9", "10",],
+				"fruitname":"pecans",
+				"ripemonths": ["10"],
+			},
+			{
+				"fruitname":"persimmons",
+				"ripemonths": ["9", "10", "11"],
+			},
+			{
+				"fruitname":"pistachios",
+				"ripemonths": ["9", "10", "11"],
+			},
+			{
+				"fruitname":"plums",
+				"ripemonths": ["5", "6", "7", "8", "9", "10", "11"],
+			},
+			{
+				"fruitname":"pomegranates",
+				"ripemonths": ["9", "10", "11", "12"],
+			},
+			{
+				"fruitname":"pomelos",
+				"ripemonths": ["1", "2", "3", "4", "11", "12"],
+			},
+			{
+				"fruitname":"quinces",
+				"ripemonths": ["9", "10", "11"],
+			},
+			{
+				"fruitname":"raspberries",
+				"ripemonths": ["5", "6", "7", "8", "9", "10", "11"],
+			},
+			{
+				"fruitname":"rhubard",
+				"ripemonths": ["4", "5", "6", "7"],
+			},
+			{
+				"fruitname":"strawberries",
+				"ripemonths": ["3", "4", "5", "6", "7", "8", "9", "10", "11"],
+			},
+			{
+				"fruitname":"tayberries",
+				"ripemonths": ["6", "7", "8"],
+			},
+			{
+				"fruitname":"tomatoes",
+				"ripemonths": ["6", "7", "8", "9", "10", "11"],
+			},
+			{
+				"fruitname":"walnuts",
+				"ripemonths": ["10"],
 			},
 ]}
 
