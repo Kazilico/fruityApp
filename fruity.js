fb679c62ac27ff1cb7a007340bb7ad22e8a0328c
Josh Carrico <jcarrico138@gmail.com>
2017-03-17 23:13:32 -0400

    Introducing render to the JS.

--- fruity.js
+++ fruity.js
@@ -1,142 +1,132 @@
-/*
+var fruits = [
+	 		
+	{
+		"name":"mulberries",
+		"ripeMonths": ["07", "08"],
+		"isRipe": false, 
+	},
+	{
+		"name":"nectarines",
+		"ripeMonths": ["05", "06", "07", "08", "09", "10"],
+		"isRipe": false,
+	},
+	{
+		"name":"oranges",
+		"ripeMonths": ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+		"isRipe": false,
+	},
+	{
+		"name":"peaches",
+		"ripeMonths": ["05", "06", "07", "08", "09", "10"],
+		"isRipe": false,
+	},
+	{
+		"name":"pecans",
+		"ripeMonths": ["10"],
+		"isRipe": false,
+	},
+	{
+		"name":"persimmons",
+		"ripeMonths": ["09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"pistachios",
+		"ripeMonths": ["09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"plums",
+		"ripeMonths": ["05", "06", "07", "08", "09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"pomegranates",
+		"ripeMonths": ["09", "10", "11", "12"],
+		"isRipe": false,
+	},
+	{
+		"name":"pomelos",
+		"ripeMonths": ["01", "02", "03", "04", "11", "12"],
+		"isRipe": false,
+	},
+	{
+		"name":"quinces",
+		"ripeMonths": ["09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"raspberries",
+		"ripeMonths": ["05", "06", "07", "08", "09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"rhubard",
+		"ripeMonths": ["04", "05", "06", "07"],
+		"isRipe": false,
+	},
+	{
+		"name":"strawberries",
+		"ripeMonths": ["03", "04", "05", "06", "07", "08", "09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"tayberries",
+		"ripeMonths": ["06", "07", "08"],
+		"isRipe": false,
+	},
+	{
+		"name":"tomatoes",
+		"ripeMonths": ["06", "07", "08", "09", "10", "11"],
+		"isRipe": false,
+	},
+	{
+		"name":"walnuts",
+		"ripeMonths": ["10"],
+		"isRipe": false,
+	},
+];
+
 var month = new Date();
 var mm = month.getMonth()+1;
 
 
-if(mm<10) {
-	mm='0'+mm
+if (mm < 10) {
+	mm = '0' + mm
 }
 
 month = mm;
-console.log (month)
-*/
 
-$(document).ready(function() {
-
-	var fruits = [
-	 		
-			{
-				"name":"mulberries",
-				"ripeMonths": [7, 8],
-				"isRipe": false, 
-			},
-			{
-				"name":"nectarines",
-				"ripeMonths": [5, 6, 7, 8, 9, 10],
-				"isRipe": false,
-			},
-			{
-				"name":"oranges",
-				"ripeMonths": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-				"isRipe": false,
-			},
-			{
-				"name":"peaches",
-				"ripeMonths": [5, 6, 7, 8, 9, 10],
-				"isRipe": false,
-			},
-			{
-				"name":"pecans",
-				"ripeMonths": [10],
-				"isRipe": false,
-			},
-			{
-				"name":"persimmons",
-				"ripeMonths": [9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"pistachios",
-				"ripeMonths": [9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"plums",
-				"ripeMonths": [5, 6, 7, 8, 9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"pomegranates",
-				"ripeMonths": [9, 10, 11, 12],
-				"isRipe": false,
-			},
-			{
-				"name":"pomelos",
-				"ripeMonths": [1, 2, 3, 4, 11, 12],
-				"isRipe": false,
-			},
-			{
-				"name":"quinces",
-				"ripeMonths": [9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"raspberries",
-				"ripeMonths": [5, 6, 7, 8, 9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"rhubard",
-				"ripeMonths": [4, 5, 6, 7],
-				"isRipe": false,
-			},
-			{
-				"name":"strawberries",
-				"ripeMonths": [3, 4, 5, 6, 7, 8, 9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"tayberries",
-				"ripeMonths": [6, 7, 8],
-				"isRipe": false,
-			},
-			{
-				"name":"tomatoes",
-				"ripeMonths": [6, 7, 8, 9, 10, 11],
-				"isRipe": false,
-			},
-			{
-				"name":"walnuts",
-				"ripeMonths": [10],
-				"isRipe": false,
-			},
-		];
-
-	var container = $("#fruits");
-	var month = (new Date().getMonth() + 1);
-
-	$.each(fruits, function(i, fruit) {
-
-		var p = $('<p>');
-		var title = "The fruit is ripe on ".concat(
-			fruit.ripeMonths.join(", ")
-
-		);
-
-		p.text(fruit.name);
-		p.prop("title", title);
-		if (fruit.ripeMonths.indexOf(month) != -1) {
-			p.addClass("highlight");
-		}
-
-	});
-
-});
-
-/*	
 for (i = 0; i < fruits.length; i++) {
-	if (fruits[i].ripeMonths.indexOf(month) > -1) {
+    if (fruits[i].ripeMonths.indexOf(month) > -1) {
 
-		(fruits[i].isRipe) = true;
-		document.querySelector(".fruit." + fruits[i].name + " .layer").classList.add("inseason");
-	} else {
-		(fruits[i].isRipe) = false;
-		
-		document.querySelector(".fruit." + fruits[i].name + " .layer").classList.remove("inseason");
-		
-	}
-
+        (fruits[i].isRipe) = true
+    } else {
+        (fruits[i].isRipe) = false
+    }
 };
-	
-console.log (fruits)
-*/
+
+function render() {
+    for (i = 0; i < fruits.length; i++) {
+        var fruit = document.createElement('div');
+        fruit.setAttribute('id', fruits[i].name);
+        
+        if (fruits[i].isRipe == true) {
+        	fruit.setAttribute('class', 'ripe_fruit');
+				} else {
+          fruit.setAttribute('class', 'fruit');
+        }
+        
+        var fruitText = document.createElement('span');
+        fruitText.setAttribute('class', 'fruitText');
+
+        var fruitTextNode = document.createTextNode(fruits[i].name);
+        fruitText.appendChild(fruitTextNode);
+
+        fruit.appendChild(fruitText);
+        document.body.appendChild(fruit);
+    }
+}
+
+render();
+
